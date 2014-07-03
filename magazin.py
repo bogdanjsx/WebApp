f6e839e6349b68fdf13a424f7643e96cd09095ac
Student User <student@cs.pub.ro>
2014-07-03 15:20:24 +0300

    Server working

--- magazin.py
+++ magazin.py
@@ -3,12 +3,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/student/osss-web/db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/student/WebApp/db.sqlite'
 db = SQLAlchemy(app)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    price = db.Column(db.Integer)
 
 @app.route('/')
 def home():
@@ -16,7 +17,7 @@ def home():
 
 @app.route('/save', methods=['POST'])
 def save():
-    product = Product(name=flask.request.form['name'])
+    product = Product(name=flask.request.form['name'], price=flask.request.form['price'])
     db.session.add(product)
     db.session.commit()
     flask.flash("Product added")
@@ -29,17 +30,25 @@ def edit(product_id):
         flask.abort(404)
     if flask.request.method == 'POST':
         product.name = flask.request.form['name']
+        product.price = flask.request.form['price']
         db.session.commit()
         return flask.redirect("/edit/%d" % product.id)
     return flask.render_template('edit.html', product=product)
 
-@app.route('/api/list')
+@app.route('/api/list', methods=['POST', 'GET'])
 def api_list():
     product_id_list = []
+    product_name_list = []
+    product_price_list = []
+
     for product in Product.query.all():
         product_id_list.append(product.id)
+        product_name_list.append(product.name)
+        product_price_list.append(product.price)
     return flask.jsonify({
        'id_list' : product_id_list,
+       'name_list' : product_name_list,
+       'price_list' : product_price_list
         })
         
 @app.route("/api/product/<int:product_id>")
